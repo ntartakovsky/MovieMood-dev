024de34dc9a9039e7c0f855731ebbc349e27483b
Neta Tartakovsky <ntartakovsky@ischool.berkeley.edu>
2023-11-16 18:44:42 +0800

    connecting api

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,23 +17,38 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # # To read file as bytes:
-    # bytes_data = uploaded_file.getvalue()
-    # st.write(bytes_data)
+    df = pd.read_csv(uploaded_file)
 
-    # # To convert to a string based IO:
-    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    # st.write(stringio)
-
-    # # To read file as string:
-    # string_data = stringio.read()
-    # st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+def get_data(url, headers, data):
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            print("successfully fetched the data")
+            print(response.json())
+        else:
+            print(f"Hello person, there's a {response.status_code} error with your request")
+        return response.json()
 
 
+url = "https://neilprabhu.mids255.com/predict"
+ 
+headers = {"Content-Type": "application/json"}
+
+arr = df.to_numpy()
+
+data = {
+    "music_list":[["Danceability","Energy","Key","Loudness","Mode","Speechiness","Acousticness","Instrumentalness","Liveness","Valence","Tempo","Time Signature"]]
+}
+
+for item in arr:
+    list_str = list(item)[-12:]
+    new_list = []
+    for num in list_str:
+        new_list.append(str(num))
+    data['music_list'].append(new_list)
+
+recs = get_data(url, headers, data)
+
+st.write(recs)
 
 
 
@@ -49,28 +64,3 @@ if uploaded_file is not None:
 
 
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
